4753f999023165c0ae221c26e5544394339e8c15
eberhardth <90506267+eberhardth@users.noreply.github.com>
2021-09-18 12:17:14 +0200

    Update musik.py

--- bot/cogs/musik.py
+++ bot/cogs/musik.py
@@ -9,6 +9,20 @@ from async_timeout import timeout
 from functools import partial
 import youtube_dl
 from youtube_dl import YoutubeDL
+import ctypes
+import ctypes.util
+ 
+print("ctypes - Find opus:")
+a = ctypes.util.find_library('opus')
+print(a)
+ 
+print("Discord - Load Opus:")
+b = discord.opus.load_opus(a)
+print(b)
+ 
+print("Discord - Is loaded:")
+c = discord.opus.is_loaded()
+print(c)
 
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
